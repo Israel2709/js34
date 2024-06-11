e624034b243fea350877d334eb5a42ab95c588ea
Israel Salinas <israel.salinas.m@gmail.com>
2024-06-10 21:11:05 -0600

    condicionales

--- s2functions/main.js
+++ s2functions/main.js
@@ -21,3 +21,94 @@ const getCharCount = (elTextoQueQuieroContar) => {
 let firstCount = getCharCount("Kodemia!!!");
 
 console.log(firstCount);
+
+/*
+    sumar dos numeros
+    restar dos numeros
+    multiplicar dos numeros
+    divir dos numeros
+    saber si un numero es par
+    */
+
+const checkIfNumberIsOddOrEven = (numberToCheck) => {
+  let numberModulus = numberToCheck % 2;
+  if (numberModulus === 0) {
+    return "El número es par";
+  } else {
+    return "El número es impar";
+  }
+};
+
+/*Condicionales*/
+
+/*If / else if / else: sirve para evaluar condiciones complejas de JavaScript, y poder generar múltiples resultados con base en el resultado de la evaluación*/
+
+//frutas, vegetales, carne
+
+/*los niños son de menos de 15 años
+  los adultos son de 15 a 50 años
+  los adultos mayores son de 50 años en adelante
+*/
+
+const checkAge = (age) => {
+  if (age < 15) {
+    return "Aún eres un niño";
+  } else if (age > 15 && age < 50) {
+    return "Ya eres un adulto";
+  } else {
+    return "ahora eres un venerable adulto mayor";
+  }
+};
+
+let ageResultOne = checkAge(65);
+console.log(ageResultOne);
+
+/*Switch: Me permite generar diferentes resultados para los distintos valores de una expresión de JavaScript*/
+
+/*switch(expression){
+  case a:
+    ...do Something
+    break;
+  case b:
+    ...do Something
+    break;
+  case c:
+    ...do Something
+    break;
+  ....
+  default:
+    ...do Something
+    break;
+}*/
+
+/* Tengo una bolsa con canicas, y se reparten entre 3 personas
+      Las canicas rojas son para Pedro
+      Las canicas azules son para Pablo
+      LAs canicas amarillas son para Paco
+      Las canicas de cualquier otro color se regresan a la bolsa
+      */
+
+const assingMarblesByColor = (marbleColor) => {
+  let owner;
+  switch (marbleColor) {
+    case "rojo":
+      owner = "esta canica es de pedro";
+      break;
+    case "azul":
+      owner = "esta canica es de pablo";
+      break;
+    case "amarillo":
+      owner = "esta canica es de paco";
+      break;
+    default:
+      owner = "esta canica no tendrá dueño";
+      break;
+  }
+  return owner;
+};
+
+let marbleOne = assingMarblesByColor("rojo");
+let marbleTwo = assingMarblesByColor("peru");
+
+console.log(marbleOne);
+console.log(marbleTwo);
